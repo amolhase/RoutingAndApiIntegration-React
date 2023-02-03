4d27020ea739c7fc814f5afb484338497e84adc3
amolhase <amolhase110@gmail.com>
2023-02-03 16:14:11 +0530

    Pagination added

--- src/Components/GallaryComponent.js
+++ src/Components/GallaryComponent.js
@@ -1,25 +1,49 @@
 import { useEffect, useState } from "react";
 import PhotoComponent from "./PhotoComponent";
-import '../Style/GallaryComponent.css'
+import "../Style/GallaryComponent.css";
+import ReactPaginate from 'react-paginate';
+
+const perPage = 50;
 
 const GallaryComponent = () => {
-    const [photosData,setPhotosData] = useState([]);
+  const [photosData, setPhotosData] = useState([]);
 
-    useEffect(()=> {
-        fetch("https://jsonplaceholder.typicode.com/photos")
-        .then(res => res.json())
-        .then(data => setPhotosData(data))
-    },[])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((data) => setPhotosData(data));
+  }, []);
 
-    return (
-        <div className="container">
-            {photosData.map((photo,index) => {
-                return (
-                     <PhotoComponent key={index} photo={photo}/>
-                )
-            })}
-        </div>
-    )
-}
+const [itemOffset, setItemOffset] = useState(0);
+const endOffset = itemOffset + perPage;
+const currentItems = photosData.slice(itemOffset, endOffset);
+const pageCount = Math.ceil(photosData.length / perPage);
+
+const handlePageClick = (event) => {
+  const newOffset = (event.selected * perPage) % photosData.length;
+  setItemOffset(newOffset);
+};
+
+  return (
+    <div>
+      <div className="container">
+        {currentItems.map((photo, index) => {
+          return <PhotoComponent key={index} photo={photo} />;
+        })}
+      </div>
+      <div>
+      <ReactPaginate
+       previousLabel={"← previous"}
+       nextLabel="next →"pageCount={pageCount}
+       onPageChange={handlePageClick}
+       containerClassName={"pagination"}
+       previousLinkClassName={"pagination_link"}
+       nextLinkClassName={"pagination_link"}
+       disabledClassName={"pagination_link--disabled"}
+       activeClassName={"pagination_link--active"} />
+      </div>
+    </div>
+  );
+};
 
 export default GallaryComponent;
--- src/Components/PhotoDetails.js
+++ src/Components/PhotoDetails.js
@@ -1,23 +1,32 @@
-import { Link , useLocation , useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import '../Style/PhotoDetails.css';
 
 const PhotoDetails = (props) => {
-    const {id} = useParams();
+  const [data, setData] = useState({});
+  const { id } = useParams();
 
-    useEffect(()=> {
-        fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
-        .then(res => res.json())
-        .then(data => console.log("data",data))
-    },[])
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
 
-
-   
-    return (
-        <div>
-            <h2>PhotoDetails {id}</h2>
-            <Link to="/">Home</Link>
+  return (
+    <div>
+      <h2 className="heading">PhotoDetails</h2>
+      <div className="details">
+        <div className="image-details">
+          <img src={data.url} alt="alt" />
         </div>
-    )
-}
+        <div className="title-details">
+            {data.title}
+        </div>
+        <Link to="/">Home</Link>
+      </div>
+      
+    </div>
+  );
+};
 
 export default PhotoDetails;
